9b37acc6d9150e9dd4626f7f980bd4c3eff6e1e0
Super-Gluten <2751242562@qq.com>
2025-07-09 09:54:44 +0800

    fix_0x01

--- src/0x01/pkg/kernel/src/utils/macros.rs
+++ src/0x01/pkg/kernel/src/utils/macros.rs
@@ -67,9 +67,7 @@ pub fn print_internal(args: Arguments) {
 #[cfg(all(target_os = "none"))]
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-    // force unlock serial for panic output
-    unsafe { SERIAL.get().unwrap().force_unlock() };
-
+    unsafe{SERIAL.get().unwrap().force_unlock()};
     error!("ERROR: panic!\n\n{:#?}", info);
     loop {}
 }
---
+++ src/0x01/test.typ
@@ -0,0 +1,13 @@
+#import "../base/report.typ": *
+
+#show: report.with(
+  title: "实验报告实验",
+  subtitle: "尝试使用 Typst 编写实验报告",
+  name: "张三",
+  stdid: "22223333",
+  classid: "实验二班",
+  major: "实验报告编写技术",
+  school: "实验报告学院",
+  time: "2022~2023 学年第二学期",
+  banner: "./images/sysu.png"
+)
