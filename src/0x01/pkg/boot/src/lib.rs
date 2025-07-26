cbb02c14ce164b150ca6829b65ae5dcd072b0b3c
Super-Gluten <2751242562@qq.com>
2025-07-26 13:04:24 +0800

    feat(0x01): 加分项3：根据内核启动参数设置log_level

--- src/0x01/pkg/boot/src/lib.rs
+++ src/0x01/pkg/boot/src/lib.rs
@@ -37,6 +37,9 @@ pub struct BootInfo {
 
     /// The system table virtual address
     pub system_table: NonNull<core::ffi::c_void>,
+
+    /// The log level
+    pub log_level: &'static str,
 }
 
 /// Get current page table from CR3
