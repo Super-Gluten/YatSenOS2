e9a24ad5c028cce621c31c96e8e4f1532aed40bc
Super-Gluten <2751242562@qq.com>
2025-07-30 08:06:53 +0800

    feat(0x02): 加分项1，为全部的CPU异常添加中断处理程序

--- src/0x02/pkg/kernel/src/interrupt/exceptions.rs
+++ src/0x02/pkg/kernel/src/interrupt/exceptions.rs
@@ -5,28 +5,159 @@ use x86_64::registers::control::Cr2;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 pub unsafe fn register_idt(idt: &mut InterruptDescriptorTable) {
-    // set_handler_fn是将特定中断、异常绑定到自定义处理函数的方法
-    // set_stack_index是为高风险异常分配独立栈空间的方法
-    idt.divide_error.set_handler_fn(divide_error_handler);
-    idt.double_fault
-        .set_handler_fn(double_fault_handler)
-        .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+    // see: https://wiki.osdev.org/Exceptions
+    //
+    // Fault:
+    idt.divide_error
+        .set_handler_fn(divide_error_handler);
+    idt.bound_range_exceeded
+        .set_handler_fn(bound_range_exceeded_handler);
+    idt.invalid_opcode
+        .set_handler_fn(invalid_opcode_handler);
+    idt.device_not_available
+        .set_handler_fn(device_not_available_handler);
+    idt.invalid_tss
+        .set_handler_fn(invalid_tss_handeler);
+    idt.segment_not_present
+        .set_handler_fn(segment_not_present_handler);
+    idt.stack_segment_fault
+        .set_handler_fn(stack_segment_fault_handler);
+    idt.general_protection_fault
+        .set_handler_fn(general_protection_fault_handler);
     idt.page_fault
         .set_handler_fn(page_fault_handler)
         .set_stack_index(gdt::PAGE_FAULT_IST_INDEX);
+    idt.x87_floating_point
+        .set_handler_fn(x87_floating_point_handler);
+    idt.alignment_check
+        .set_handler_fn(alignment_check_handler);
+    idt.simd_floating_point
+        .set_handler_fn(simd_floating_point_handler);
+    // idt.virtualization
+    //     .set_handler_fn(virtualization_handler);
+    // idt.control_protection
+    //     .set_handler_fn(control_protection_handler);
+    // idt.hypervisior_injection
+    //     .set_handler_fn(hypervisior_injection_handler);
+    // idt.vmm_communication
+    //     .set_handler_fn(vmm_communication_handler);
+    // idt.security
+    //     .set_handler_fn(security_handler);
+    
+    // Trap
+    // idt.debug
+    //     .set_handler_fn(debug_handler);
+    // idt.breakpoint
+    //     .set_handler_fn(breakpoint_handler);
+    // idt.overflow
+    //     .set_handler_fn(overflow_handler);
 
-    // TODO: you should handle more exceptions here
-    // especially general protection fault (GPF)
-    // see: https://wiki.osdev.org/Exceptions
+    // Abort
+    idt.double_fault
+        .set_handler_fn(double_fault_handler)
+        .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+    idt.machine_check
+        .set_handler_fn(machine_check_handler);
+    
+    // Other
+    // idt.non_maskable_interrupt
+    //     .set_handler_fn(non_maskable_interrupt_handler);
 
-    idt.general_protection_fault
-        .set_handler_fn(general_protection_fault_handler);
+    // Legacy
+    // idt.coprocessor_segment_overrun
+    //     .set_handler_fn(coprocessor_segment_overrun_handler);
+    
 }
-
+    
+/// fault handler
 pub extern "x86-interrupt" fn divide_error_handler(stack_frame: InterruptStackFrame) {
     panic!("EXCEPTION: DIVIDE ERROR\n\n{:#?}", stack_frame);
 }
 
+pub extern "x86-interrupt" fn bound_range_exceeded_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: BOUND RANGE EXCEEDED\n\n{:#?}", stack_frame);
+}
+
+pub extern "x86-interrupt" fn invalid_opcode_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: INVAILD OPCODE\n\n{:#?}", stack_frame);
+}
+
+pub extern "x86-interrupt" fn device_not_available_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: DEVICE NOT AVAILABLE\n\n{:#?}", stack_frame);
+}
+
+pub extern "x86-interrupt" fn invalid_tss_handeler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    panic!(
+        "EXCEPTION: INVAILD TSS, ERROR_CODE: 0x{:016x}\n\n{:#?}",
+        error_code, stack_frame
+    );
+}
+
+pub extern "x86-interrupt" fn segment_not_present_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    panic!(
+        "EXCEPTION: SEGMENT NOT PRESENT, ERROR_CODE: 0x{:016x}\n\n{:#?}",
+        error_code, stack_frame
+    );
+}
+
+pub extern "x86-interrupt" fn stack_segment_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    panic!(
+        "EXCEPTION: STACK SEGMENT FAULT, ERROR_CODE: 0x{:016x}\n\n{:#?}",
+        error_code, stack_frame
+    );
+}
+
+pub extern "x86-interrupt" fn general_protection_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) {
+    panic!(
+        "EXCEPTION: GENERAL PROTECTION FAULT, ERROR_CODE: 0x{:016x}\n\n{:#?}",
+        error_code, stack_frame
+    );
+}
+
+pub extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    panic!(
+        "EXCEPTION: PAGE FAULT, ERROR_CODE: {:?}\n\nTrying to access: {:#x}\n{:#?}",
+        error_code,
+        Cr2::read().unwrap_or(VirtAddr::new_truncate(0xdeadbeef)),
+        stack_frame
+    );
+}
+
+pub extern "x86-interrupt" fn x87_floating_point_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: X87 FLOATING POINT\n\n{:#?}", stack_frame);
+}
+
+pub extern "x86-interrupt" fn alignment_check_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) {
+    panic!(
+        "EXCEPTION: ALIGNMENT CHECK, ERROR_CODE: 0\n\n{:#?}",
+        stack_frame
+    );
+}
+
+pub extern "x86-interrupt" fn simd_floating_point_handler(stack_frame: InterruptStackFrame) {
+    panic!("EXCEPTION: SIMD FLOATING POINT\n\n{:#?}", stack_frame);
+}
+
+
+// abort
 pub extern "x86-interrupt" fn double_fault_handler(
     stack_frame: InterruptStackFrame,
     error_code: u64,
@@ -37,25 +168,6 @@ pub extern "x86-interrupt" fn double_fault_handler(
     );
 }
 
-pub extern "x86-interrupt" fn page_fault_handler(
-    stack_frame: InterruptStackFrame,
-    err_code: PageFaultErrorCode,
-) {
-    panic!(
-        "EXCEPTION: PAGE FAULT, ERROR_CODE: {:?}\n\nTrying to access: {:#x}\n{:#?}",
-        err_code,
-        Cr2::read().unwrap_or(VirtAddr::new_truncate(0xdeadbeef)),
-        stack_frame
-    );
-}
-
-pub extern "x86-interrupt" fn general_protection_fault_handler(
-    stack_frame: InterruptStackFrame,
-    err_code: u64,
-    // 在 idt.rs中expected为u64而非PageFaultErrorCode
-) {
-    panic!(
-        "EXCEPTION: GENERAL PROTECTION FAULT, ERROR_CODE: 0x{:016x}\n\n{:#?}",
-        err_code, stack_frame
-    );
+pub extern "x86-interrupt" fn machine_check_handler(stack_frame: InterruptStackFrame) -> ! {
+    panic!("EXCEPTION: MACHINE CHECK\n\n{:#?}", stack_frame);
 }
