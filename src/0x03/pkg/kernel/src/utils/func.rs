cf9ee769129c4c621d8575c0c6241dfb07eb4cd5
Super-Gluten <2751242562@qq.com>
2025-08-06 12:16:58 +0800

    feat(0x03): 展示多个test下进程切换后id的显示

--- src/0x03/pkg/kernel/src/utils/func.rs
+++ src/0x03/pkg/kernel/src/utils/func.rs
@@ -1,18 +1,37 @@
+use lazy_static::lazy_static;
+use alloc::string::String;
+use alloc::format;
+use spin::Mutex;
+
+lazy_static! {
+    static ref STRING: Mutex<String> = Mutex::new(String::new());
+}
+
+const MAX_STRING_LENGTH: usize = 100;
+
 pub fn test() -> ! {
-    // 简单的无限循环计数器，发散函数
     let mut count = 0;
     let id;
     if let Some(id_env) = crate::proc::env("id") {
-        id = id_env // 尝试从环境变量中获取id
+        id = id_env
     } else {
-        id = "unknown".into() // 否则赋值为"unknown"
+        id = "unknown".into()
     }
     loop {
         // TODO: better way to show more than one process is running?
         count += 1;
         if count == 10000 {
             count = 0;
-            print!("\r{:-6} => Tick!", id); // 每1000次循环，打印ID和 Tick信息
+            let mut string = STRING.lock();
+            let add_str = format!("{} ", id);
+
+            if string.len() > MAX_STRING_LENGTH {
+                println!();
+                string.clear();
+            }
+
+            string.push_str(&add_str);
+            print!("{}", add_str);
         }
         unsafe {
             x86_64::instructions::hlt();
@@ -33,7 +52,7 @@ fn huge_stack() {
     for i in 0..stack.len() / 256 {
         println!("{:#05x} == {:#05x}", i * 256, stack[i * 256]);
     }
-} // 用于测试栈的分配（栈是否大小足够且访问正确）
+}
 
 pub fn stack_test() -> ! {
     huge_stack();
