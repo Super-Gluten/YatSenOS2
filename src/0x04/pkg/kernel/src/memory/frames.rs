ade6c238b69d67355b10182ca34af3ad56ee907e
Super-Gluten <2751242562@qq.com>
2025-08-15 09:46:44 +0800

    feat(0x04): frames回收数量统计函数

--- src/0x04/pkg/kernel/src/memory/frames.rs
+++ src/0x04/pkg/kernel/src/memory/frames.rs
@@ -42,6 +42,10 @@ impl BootInfoFrameAllocator {
     pub fn frames_total(&self) -> usize {
         self.size
     }
+
+    pub fn frames_recycle(&self) -> usize {
+        self.free_frames.len()
+    }
 }
 
 unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
